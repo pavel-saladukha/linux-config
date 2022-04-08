d6e60d30d83f11bd005b43be184573700a7e41ec
Pavel Saladukha <pavel.soloduha@outlook.com>
2022-04-08 20:58:54 +0200

    Init

---
+++ root.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+#Remove everything unused
+
+dnf remove @libreoffice -y
+dnf group remove libreoffice -y
+
+dnf remove -y \
+	libreoffice-* \
+	totem \
+	rhythmbox \
+	gnome-boxes \
+	gnome-contacts \
+	gnome-connections
+
+dnf autoremove -y
+
+#Upgrade
+dnf check-update
+dnf upgrade -y
+dnf autoremove -y
+
+#DNF
+dnf install -y \
+	vim-enhanced \
+	inxi \
+	htop \
+	unzip \
+	redhat-lsb \
+	neofetch \
+	stress \
+	stacer \
+	sysprof \
+	thinkfan \
+	alacritty \
+	dnf-plugins-core \
+	rclone \
+	rclone-browser
+	
+#Python3
+dnf install -y \
+	python3-virtualenv \
+	python3-pip
+
+#Virtualization
+dnf group install --with-optional -y \
+	virtualization \
+	vagrant 
+
+usermod -aG libvirt ${SUDO_USER}
+service libvirtd.service enable
+service libvirtd.service start
+
+#Terraform
+# dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+# dnf install -y \
+# 	terraform
+
+cat <<EOF | tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+dnf install -y \
+	kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-latest.x86_64.rpm
+rpm -Uvh minikube-latest.x86_64.rpm
+rm -f minikube-latest.x86_64.rpm
+
+wget https://github.com/apptainer/apptainer/releases/download/v1.0.1/apptainer-1.0.1-1.x86_64.rpm
+rpm -Uvh apptainer-1.0.1-1.x86_64.rpm
+rm -f apptainer-1.0.1-1.x86_64.rpm
+
+rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf install -y \
+	sublime-text \
+	sublime-merge
+
+dnf check-update
+dnf upgrade -y
+dnf autoremove -y
+
+#FLATPAK
+flatpak update -y
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak update -y
+
+flatpak install flathub -y \
+	com.github.maoschanz.drawing \
+	org.gimp.GIMP \
+	com.rafaelmardojai.Blanket \
+	de.haeckerfelix.Shortwave \
+	org.flameshot.Flameshot \
+	io.github.seadve.Kooha \
+	io.github.seadve.Mousai \
+	org.videolan.VLC \
+	org.libreoffice.LibreOffice \
+	org.qbittorrent.qBittorrent \
+	org.gnome.DejaDup \
+	com.calibre_ebook.calibre
+
+flatpak install flathub -y \
+	com.mattjakeman.ExtensionManager \
+	fr.romainvigier.MetadataCleaner \
+	org.gustavoperedo.FontDownloader
+
+flatpak install flathub -y \
+	com.todoist.Todoist \
+	org.gnome.GTG
+
+flatpak install flathub -y \
+	org.mozilla.Thunderbird \
+	com.google.Chrome \
+	org.chromium.Chromium
+
+flatpak install flathub -y \
+	com.skype.Client \
+	com.viber.Viber \
+	org.telegram.desktop
+
+# flatpak install flathub com.ulduzsoft.Birdtray
+# flatpak install flathub -y \
+# 	org.texstudio.TeXstudio
+
+flatpak update -y
---
+++ user.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+mkdir -p ${HOME}/Soft/{java17,java11,java8,maven,gradle,groovy}
+
+#Ansible
+python -m pip install --user ansible
+
+#SDKMan
+curl -s "https://get.sdkman.io" | bash 
+source "${HOME}/.sdkman/bin/sdkman-init.sh"
+
+sdk install java 17.0.2-open ${HOME}/Soft/java17
+sdk install java 11.0.12-open ${HOME}/Soft/java11
+sdk install java 8.0.302-open ${HOME}/Soft/java8
+
+sdk install maven 3.8.5 ${HOME}/Soft/maven
+
+sdk install gradle 7.4.2 ${HOME}/Soft/gradle
+
+sdk install groovy 4.0.1 ${HOME}/Soft/groovy
+
+#AWS CLI
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm awscliv2.zip
+rm -rf aws
+
+#JetBrains
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.23.11680.tar.gz 
+tar -xf jetbrains-toolbox-1.23.11680.tar.gz -C ${HOME}/Soft
+mv ${HOME}/Soft/jetbrains-toolbox-1.23.11680 ${HOME}/Soft/jetbrains-toolbox
+rm -f jetbrains-toolbox-1.23.11680.tar.gz
+
+cp ./.vimrc ${HOME}/.vimrc
+mkdir -p ${HOME}/.bash.d
+cd bash_aliases.d
+cp -R bash_aliases.d/ ${HOME}/.bash.d/
+
+git config --global core.editor "subl -n -w"
+
+#install extensions
+mkdir -p ${HOME}/ge
+wget https://github.com/stuarthayhurst/alphabetical-grid-extension/releases/download/v22.0/AlphabeticalAppGrid@stuarthayhurst.shell-extension.zip -O ${HOME}/ge/alphabetical-grid.zip
+wget https://github.com/aunetx/blur-my-shell/releases/download/v28/blur-my-shell@aunetx.zip -O ${HOME}/ge/blur-my-shell.zip
+wget https://github.com/eonpatapon/gnome-shell-extension-caffeine/archive/refs/tags/v41.zip -O ${HOME}/ge/caffeine.zip
+wget https://github.com/home-sweet-gnome/dash-to-panel/releases/download/v46/dash-to-panel@jderose9.github.com_v46.zip -O ${HOME}/ge/dash-to-panel.zip
+wget https://github.com/kgshank/gse-sound-output-device-chooser/archive/refs/tags/39.zip -O ${HOME}/ge/sound-output-device-chooser.zip
+wget https://github.com/MartinPL/Tray-Icons-Reloaded/releases/download/23/trayIconsReloaded@selfmade.pl.zip -O ${HOME}/ge/tray-icons.zip
+wget https://github.com/G-dH/workspace-switcher-manager/releases/download/wsm-v3/workspace-switcher-manager@G-dH.github.com.zip -O ${HOME}/ge/workspace-switcher.zip
+ls ${HOME}/ge | xargs gnome-extensions install ${HOME}/ge/
+rm -rf ${HOME}/ge
